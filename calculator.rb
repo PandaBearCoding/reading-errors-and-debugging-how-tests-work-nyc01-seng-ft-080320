bf8756c5f3cf763a67ece1a6e319cc082617fb92
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-27 20:09:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,8 +2,8 @@
 
 first_number = 42
 second_number = 2.3
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number ** second_number
-quotient = first_number / second_number
+sum = first_number+second_number
+difference = first_number-second_number
+product = first_number*second_number
+quotient = first_number/second_number
 
